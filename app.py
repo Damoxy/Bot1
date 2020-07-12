45e43125a9dd5ebbf2a6f5d555d2cd25c4adf22e
Adedamola Obadeji <40131855+Damoxy@users.noreply.github.com>
2020-07-12 14:05:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,7 @@ def check():
     df=pd.read_csv('result.csv')
     prediksi=prediction(df)
     return render_template('index.html',prediction=prediksi)
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
     
